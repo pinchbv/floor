7acb9927c35c2a2f3381cc7bf8187aadbc651134
Vitus Ortner <vitusortner.dev@gmail.com>
2019-04-14 19:39:58 +0200

    Foo

--- floor_generator/test/misc/string_utils_test.dart
+++ floor_generator/test/misc/string_utils_test.dart
@@ -2,11 +2,11 @@ import 'package:floor_generator/misc/string_utils.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('decapitalize word', () {
-    const string = 'Foo';
+  test('decapitalize word (first letter to lowercase)', () {
+    const string = 'FOO';
 
     final actual = decapitalize(string);
 
-    expect(actual, 'foo');
+    expect(actual, 'fOO');
   });
 }
