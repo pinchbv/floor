dd5f2c74ea32a41359a4692b59dbef87c7b965e2
vitusortner <vitusortner.dev@gmail.com>
2019-05-19 15:04:42 +0200

    Add Field tests

---
+++ floor_generator/test/value_object/field_test.dart
@@ -0,0 +1,48 @@
+import 'package:floor_generator/misc/constants.dart';
+import 'package:floor_generator/value_object/field.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import '../mocks.dart';
+
+void main() {
+  final mockFieldElement = MockFieldElement();
+
+  tearDown(() {
+    clearInteractions(mockFieldElement);
+    reset(mockFieldElement);
+  });
+
+  test('Get database definition with auto generate primary key', () {
+    const autoGenerate = true;
+    final field = Field(
+      mockFieldElement,
+      'field1Name',
+      'field1ColumnName',
+      false,
+      SqlType.INTEGER,
+    );
+
+    final actual = field.getDatabaseDefinition(autoGenerate);
+
+    final expected =
+        '`${field.columnName}` ${field.sqlType} PRIMARY KEY AUTOINCREMENT NOT NULL';
+    expect(actual, equals(expected));
+  });
+
+  test('Get database definition', () {
+    const autoGenerate = false;
+    final field = Field(
+      mockFieldElement,
+      'field1Name',
+      'field1ColumnName',
+      true,
+      SqlType.TEXT,
+    );
+
+    final actual = field.getDatabaseDefinition(autoGenerate);
+
+    final expected = '`${field.columnName}` ${field.sqlType}';
+    expect(actual, equals(expected));
+  });
+}
