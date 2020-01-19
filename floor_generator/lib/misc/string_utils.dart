6423a6ed32ba420631374c35e93986e8b527198c
vitusortner <vitusortner.dev@gmail.com>
2020-01-19 20:49:07 +0100

    Remove unused extension function

--- floor_generator/lib/misc/string_utils.dart
+++ floor_generator/lib/misc/string_utils.dart
@@ -2,9 +2,3 @@
 String decapitalize(final String value) {
   return '${value[0].toLowerCase()}${value.substring(1)}';
 }
-
-extension on String {
-  String decapitalize() {
-    return '${this[0].toLowerCase()}${substring(1)}';
-  }
-}
