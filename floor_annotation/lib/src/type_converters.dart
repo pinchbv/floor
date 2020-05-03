901a9cbd4207c60a7462f638bbc05ade4b7e8383
vitusortner <vitusortner.dev@gmail.com>
2020-05-03 22:30:12 +0200

    Update type converts annotation documentation

--- floor_annotation/lib/src/type_converters.dart
+++ floor_annotation/lib/src/type_converters.dart
@@ -1,9 +1,16 @@
 /// Specifies additional type converters that Floor can use.
 /// The TypeConverter is added to the scope of the element so if you put it on
-/// a class / interface, all methods / fields in that class will be able to use
-/// the converters.
+/// a class, all methods/fields in that class will be able to use the
+/// converters.
+///
+/// Type converters can be applied to:
+/// 1. databases
+/// 1. DAOs
+/// 1. entities
+/// 1. entity fields
+/// 1. DAO methods
+/// 1. DAO method parameters
 class TypeConverters {
-  // TODO #165 update documentation
   /// The list of type converter classes.
   final List<Type> value;
 
