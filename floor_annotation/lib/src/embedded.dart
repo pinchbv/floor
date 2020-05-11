bba6d2f3eaf55d8e701f89e6d6922ac98d4324bd
Huthaifah Mustafa <7uthaifah.mustafa@gmail.com>
2020-05-11 18:20:06 +0300

    Add @Embedded annotation

---
+++ floor_annotation/lib/src/embedded.dart
@@ -0,0 +1,9 @@
+class Embedded {
+  /// Used for the fields of the embedded object.
+  final String prefix;
+
+  /// By default, prefix is empty.
+  const Embedded({this.prefix = ''});
+}
+
+const embedded = Embedded();
