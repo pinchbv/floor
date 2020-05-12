81ebd780dda7d39cf15e362c6fccb3e66705b7a6
vitusortner <vitusortner.dev@gmail.com>
2020-05-12 21:12:09 +0200

    Rename and restructure type utils tests

--- floor_generator/test/misc/type_utils_test.dart
+++ floor_generator/test/misc/type_utils_test.dart
@@ -6,62 +6,65 @@ import 'package:test/test.dart';
 import '../test_utils.dart';
 
 void main() {
-  group('assert types', () {
-    // TODO #165 rename tests
-    test('string is supported type', () async {
-      final type = await getDartType("'foo bar'");
+  group('Type assertions', () {
+    group('default SQL type assertions', () {
+      test('string is default SQL type', () async {
+        final type = await getDartType("'foo bar'");
 
-      final actual = type.isDefaultSqlType;
+        final actual = type.isDefaultSqlType;
 
-      expect(actual, isTrue);
+        expect(actual, isTrue);
+      });
+
+      test('bool is default SQL type', () async {
+        final type = await getDartType(true);
+
+        final actual = type.isDefaultSqlType;
+
+        expect(actual, isTrue);
+      });
+
+      test('int is default SQL type', () async {
+        final type = await getDartType(1);
+
+        final actual = type.isDefaultSqlType;
+
+        expect(actual, isTrue);
+      });
+
+      test('double is default SQL type', () async {
+        final type = await getDartType(1.1);
+
+        final actual = type.isDefaultSqlType;
+
+        expect(actual, isTrue);
+      });
+
+      test('Uint8List is default SQL type', () async {
+        final type = await getDartType(Uint8List(10));
+
+        final actual = type.isDefaultSqlType;
+
+        expect(actual, isTrue);
+      });
     });
 
-    test('bool is supported type', () async {
-      final type = await getDartType(true);
+    group('stream assertion', () {
+      test('is stream', () async {
+        final type = await getDartTypeFromString('Stream<String>.empty()');
 
-      final actual = type.isDefaultSqlType;
+        final actual = type.isStream;
 
-      expect(actual, isTrue);
-    });
+        expect(actual, isTrue);
+      });
 
-    test('int is supported type', () async {
-      final type = await getDartType(1);
+      test('is not stream', () async {
+        final type = await getDartType(1);
 
-      final actual = type.isDefaultSqlType;
+        final actual = type.isStream;
 
-      expect(actual, isTrue);
-    });
-
-    test('double is supported type', () async {
-      final type = await getDartType(1.1);
-
-      final actual = type.isDefaultSqlType;
-
-      expect(actual, isTrue);
-    });
-
-    test('Uint8List is supported type', () async {
-      final type = await getDartType(Uint8List(10));
-
-      final actual = type.isDefaultSqlType;
-
-      expect(actual, isTrue);
-    });
-
-    test('is stream', () async {
-      final type = await getDartTypeFromString('Stream<String>.empty()');
-
-      final actual = type.isStream;
-
-      expect(actual, isTrue);
-    });
-
-    test('is not stream', () async {
-      final type = await getDartType(1);
-
-      final actual = type.isStream;
-
-      expect(actual, isFalse);
+        expect(actual, isFalse);
+      });
     });
   });
 
