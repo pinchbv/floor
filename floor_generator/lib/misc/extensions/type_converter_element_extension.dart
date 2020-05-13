294212e9b2584fa63136bd8c973bdb3566a6e976
vitusortner <vitusortner.dev@gmail.com>
2020-05-13 22:41:41 +0200

    Throw when type converters annotation is misused

--- floor_generator/lib/misc/extensions/type_converter_element_extension.dart
+++ floor_generator/lib/misc/extensions/type_converter_element_extension.dart
@@ -1,27 +1,53 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:floor_annotation/floor_annotation.dart' as annotations;
+import 'package:floor_generator/misc/annotations.dart';
 import 'package:floor_generator/misc/constants.dart';
 import 'package:floor_generator/misc/type_utils.dart';
 import 'package:floor_generator/processor/type_converter_processor.dart';
 import 'package:floor_generator/value_object/type_converter.dart';
-
-import '../annotations.dart';
+import 'package:source_gen/source_gen.dart';
 
 extension TypeConverterElementExtension on Element {
   @nonNull
   List<TypeConverter> getTypeConverters(final TypeConverterScope scope) {
     if (hasAnnotation(annotations.TypeConverters)) {
-      return getAnnotation(annotations.TypeConverters)
-              .getField(AnnotationField.typeConverterValue)
-              ?.toListValue()
-              ?.map((object) => object.toTypeValue().element)
-              ?.whereType<ClassElement>() // TODO #165 throw when not class
-              ?.where((element) =>
-                  element.isTypeConverter) // TODO #165 throw when not
-              ?.map(
-                  (element) => TypeConverterProcessor(element, scope).process())
-              ?.toList() ??
-          [];
+      final typeConverterElements = getAnnotation(annotations.TypeConverters)
+          .getField(AnnotationField.typeConverterValue)
+          ?.toListValue()
+          ?.map((object) => object.toTypeValue().element);
+
+      if (typeConverterElements == null || typeConverterElements.isEmpty) {
+        throw InvalidGenerationSourceError(
+            'There are no type converts defined even though the @TypeConverters annotation is used.',
+            todo: 'Supply a type converter class to the annotation.',
+            element: this);
+      }
+
+      if (typeConverterElements.any((element) => element is! ClassElement)) {
+        throw InvalidGenerationSourceError(
+          'Only classes can be used as type converters.',
+          todo: 'Make sure use a class that inherits from TypeConverter.',
+          element: this,
+        );
+      }
+
+      final typeConverterClassElements =
+          typeConverterElements.cast<ClassElement>();
+
+      if (typeConverterClassElements
+          .any((element) => !element.isTypeConverter)) {
+        throw InvalidGenerationSourceError(
+          'Only classes that inherit from TypeConverter can be used as type converters.',
+          todo: 'Make sure use a class that inherits from TypeConverter.',
+          element: this,
+        );
+      }
+
+      final typeConverters = typeConverterClassElements
+          .map((element) => TypeConverterProcessor(element, scope).process())
+          .toList();
+
+      return typeConverters;
     } else {
       return [];
     }
