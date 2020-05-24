ddf88a9af1e6ad378073944bbb4acf626a78434c
vitusortner <vitusortner.dev@gmail.com>
2020-05-24 10:18:17 +0200

    Add documentation

--- floor_generator/lib/misc/extension/type_converters_extension.dart
+++ floor_generator/lib/misc/extension/type_converters_extension.dart
@@ -5,12 +5,15 @@ import 'package:floor_generator/value_object/type_converter.dart';
 import 'package:source_gen/source_gen.dart';
 
 extension TypeConvertersExtension on Iterable<TypeConverter> {
+  /// Returns the [TypeConverter] in the closest [TypeConverterScope] or null
   @nullable
   TypeConverter get closestOrNull {
     return sortedByDescending((typeConverter) => typeConverter.scope.index)
         .firstOrNull;
   }
 
+  /// Returns the [TypeConverter] in the closest [TypeConverterScope] for
+  /// [dartType] or null
   @nullable
   TypeConverter getClosestOrNull(DartType dartType) {
     return sortedByDescending((typeConverter) => typeConverter.scope.index)
@@ -18,6 +21,8 @@ extension TypeConvertersExtension on Iterable<TypeConverter> {
             (typeConverter) => typeConverter.fieldType == dartType);
   }
 
+  /// Returns the [TypeConverter] in the closest [TypeConverterScope] for
+  /// [dartType]
   @nonNull
   TypeConverter getClosest(DartType dartType) {
     final closest = getClosestOrNull(dartType);
