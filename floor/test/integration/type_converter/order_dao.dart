4e5a5f789fab744817c3e471344a4ebea8035a9f
vitusortner <vitusortner.dev@gmail.com>
2020-06-13 12:20:48 +0200

    Get rid of unused DAO methods

--- floor/test/integration/type_converter/order_dao.dart
+++ floor/test/integration/type_converter/order_dao.dart
@@ -7,18 +7,6 @@ abstract class OrderDao {
   @insert
   Future<void> insertOrder(Order order);
 
-  @update
-  Future<void> updateOrder(Order order);
-
-  @delete
-  Future<void> deleteOrder(Order order);
-
-  @Query('SELECT * FROM `Order` WHERE id = :id')
-  Future<Order> findOrderById(int id);
-
   @Query('SELECT * FROM `Order` WHERE date = :date')
   Future<List<Order>> findOrdersByDate(DateTime date);
-
-  @Query('SELECT * FROM `Order`')
-  Future<List<Order>> findAllOrders();
 }
