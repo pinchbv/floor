6ac81114f04e379529bc4ba9293a4f0b09493e9c
Markus Richter <8398165+mqus@users.noreply.github.com>
2020-08-16 01:40:41 +0200

    Add comment to pinned analyzer version and add more tests to daowriter

--- floor_generator/test/writer/dao_writer_test.dart
+++ floor_generator/test/writer/dao_writer_test.dart
@@ -305,4 +305,166 @@ void main() {
       }
     '''));
   });
+
+  test(
+      'create DAO with altering query, QueryAdapter doesn\'t need changeListener',
+      () async {
+    final dao = await createDao('''
+      @dao
+      abstract class PersonDao {
+        @Query("DELETE FROM Person")
+        Future<void> deleteAllPersons();        
+        
+        @insert
+        Future<void> insertPerson(Person person);
+        
+        @update
+        Future<void> updatePerson(Person person);
+        
+        @delete
+        Future<void> deletePerson(Person person);
+      }
+    ''');
+    final actual = DaoWriter(dao, {'Dog'}).write();
+
+    expect(actual, equalsDart(r'''
+      class _$PersonDao extends PersonDao {
+        _$PersonDao(this.database, this.changeListener)
+            :  _queryAdapter = QueryAdapter(database),
+               _personInsertionAdapter = InsertionAdapter(
+                  database,
+                  'Person',
+                  (Person item) =>
+                      <String, dynamic>{'id': item.id, 'name': item.name}),
+              _personUpdateAdapter = UpdateAdapter(
+                  database,
+                  'Person',
+                  ['id'],
+                  (Person item) =>
+                      <String, dynamic>{'id': item.id, 'name': item.name}),
+              _personDeletionAdapter = DeletionAdapter(
+                  database,
+                  'Person',
+                  ['id'],
+                  (Person item) =>
+                      <String, dynamic>{'id': item.id, 'name': item.name});
+      
+        final sqflite.DatabaseExecutor database;
+      
+        final StreamController<String> changeListener;
+      
+        final QueryAdapter _queryAdapter;
+      
+        final InsertionAdapter<Person> _personInsertionAdapter;
+      
+        final UpdateAdapter<Person> _personUpdateAdapter;
+      
+        final DeletionAdapter<Person> _personDeletionAdapter;
+
+        @override
+        Future<void> deleteAllPersons() async {
+          await _queryAdapter
+              .queryNoReturn('DELETE FROM Person', changedEntities: {'Person'});
+        }
+        
+        @override
+        Future<void> insertPerson(Person person) async {
+          await _personInsertionAdapter.insert(person, OnConflictStrategy.abort);
+        }
+        
+        @override
+        Future<void> updatePerson(Person person) async {
+          await _personUpdateAdapter.update(person, OnConflictStrategy.abort);
+        }
+        
+        @override
+        Future<void> deletePerson(Person person) async {
+          await _personDeletionAdapter.delete(person);
+        }
+      }
+    '''));
+  });
+
+  test(
+      'create DAO with altering query, QueryAdapter does need a changeListener',
+      () async {
+    final dao = await createDao('''
+      @dao
+      abstract class PersonDao {
+        @Query("DELETE FROM Person")
+        Future<void> deleteAllPersons();        
+        
+        @insert
+        Future<void> insertPerson(Person person);
+        
+        @update
+        Future<void> updatePerson(Person person);
+        
+        @delete
+        Future<void> deletePerson(Person person);
+      }
+    ''');
+    // simulate DB is aware of another streamed Entity('Dog') and no View
+    final actual = DaoWriter(dao, {'Person'}).write();
+
+    expect(actual, equalsDart(r'''
+      class _$PersonDao extends PersonDao {
+        _$PersonDao(this.database, this.changeListener)
+            :  _queryAdapter = QueryAdapter(database, changeListener),
+               _personInsertionAdapter = InsertionAdapter(
+                  database,
+                  'Person',
+                  (Person item) =>
+                      <String, dynamic>{'id': item.id, 'name': item.name},
+                  changeListener),
+              _personUpdateAdapter = UpdateAdapter(
+                  database,
+                  'Person',
+                  ['id'],
+                  (Person item) =>
+                      <String, dynamic>{'id': item.id, 'name': item.name},
+                  changeListener),
+              _personDeletionAdapter = DeletionAdapter(
+                  database,
+                  'Person',
+                  ['id'],
+                  (Person item) =>
+                      <String, dynamic>{'id': item.id, 'name': item.name},
+                  changeListener);
+      
+        final sqflite.DatabaseExecutor database;
+      
+        final StreamController<String> changeListener;
+      
+        final QueryAdapter _queryAdapter;
+      
+        final InsertionAdapter<Person> _personInsertionAdapter;
+      
+        final UpdateAdapter<Person> _personUpdateAdapter;
+      
+        final DeletionAdapter<Person> _personDeletionAdapter;
+
+        @override
+        Future<void> deleteAllPersons() async {
+          await _queryAdapter
+              .queryNoReturn('DELETE FROM Person', changedEntities: {'Person'});
+        }
+   
+        @override
+        Future<void> insertPerson(Person person) async {
+          await _personInsertionAdapter.insert(person, OnConflictStrategy.abort);
+        }
+        
+        @override
+        Future<void> updatePerson(Person person) async {
+          await _personUpdateAdapter.update(person, OnConflictStrategy.abort);
+        }
+        
+        @override
+        Future<void> deletePerson(Person person) async {
+          await _personDeletionAdapter.delete(person);
+        }
+      }
+    '''));
+  });
 }
