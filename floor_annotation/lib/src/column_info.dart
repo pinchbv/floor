4502f775d71fc3aac4db70e17fb4c0f3efdcb8ee
Vitus <vitusortner.dev@gmail.com>
2020-11-20 12:11:30 +0100

    Make floor_annotation null safe (#431)

--- floor_annotation/lib/src/column_info.dart
+++ floor_annotation/lib/src/column_info.dart
@@ -1,7 +1,7 @@
 /// Allows customization of the column associated with this field.
 class ColumnInfo {
   /// The custom name of the column.
-  final String name;
+  final String? name;
 
   /// Defines if the associated column is allowed to contain 'null'.
   final bool nullable;
