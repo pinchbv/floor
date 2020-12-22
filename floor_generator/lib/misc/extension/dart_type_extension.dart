1b7882c2f0ac5026aece9cacf613ba75a87e0274
vitusortner <vitusortner.dev@gmail.com>
2020-12-22 22:07:20 +0100

    Use backwards compatible nullability inference

--- floor_generator/lib/misc/extension/dart_type_extension.dart
+++ floor_generator/lib/misc/extension/dart_type_extension.dart
@@ -1,19 +1,18 @@
 // TODO #375 delete once dependencies have migrated
 // ignore_for_file: import_of_legacy_library_into_null_safe
+import 'package:analyzer/dart/element/nullability_suffix.dart';
 import 'package:analyzer/dart/element/type.dart';
 
 extension DartTypeExtension on DartType {
-  // TODO #375 test and decide for one implementation
+  // TODO #375 test
   bool get isNullable {
-    return element.library.typeSystem.isNullable(this);
-
-    // final nullabilitySuffix = this.nullabilitySuffix;
-    // switch (nullabilitySuffix) {
-    //   case NullabilitySuffix.question:
-    //   case NullabilitySuffix.star: // support legacy code without non-nullables
-    //     return true;
-    //   case NullabilitySuffix.none:
-    //     return false;
-    // }
+    final nullabilitySuffix = this.nullabilitySuffix;
+    switch (nullabilitySuffix) {
+      case NullabilitySuffix.question:
+      case NullabilitySuffix.star: // support legacy code without non-nullables
+        return true;
+      case NullabilitySuffix.none:
+        return false;
+    }
   }
 }
