831bc7991e91d1d5c670f8e11ef59cb53762c61c
vitusortner <vitusortner.dev@gmail.com>
2021-03-07 17:39:03 +0100

    Ignore generated mocks

--- tool/generate_test_database.sh
+++ tool/generate_test_database.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd ../floor
-flutter packages pub run build_runner build --delete-conflicting-outputs
-# TODO #375 ignore mock files --build-filter=test/integration/autoincrement/autoinc_test.dart
+flutter packages pub run build_runner build --delete-conflicting-outputs --build-filter="test/integration/**/*.g.dart"
