324ccdc57edd58f2f02abf9650aefd6f395a9b3c
Tobias Ottenweller <tobi@ottenweller.net>
2021-06-08 11:41:39 +0200

    Update unit tests for database writer

--- floor_generator/test/writer/database_writer_test.dart
+++ floor_generator/test/writer/database_writer_test.dart
@@ -47,161 +47,47 @@ void main() {
               await callback?.onOpen?.call(database);
             },
             onUpgrade: (database, startVersion, endVersion) async {
-              await MigrationAdapter.runMigrations(
-                  database, startVersion, endVersion, migrations);
-
+              await _migrate(
+                  database, migrations, startVersion, endVersion, callback);
               await callback?.onUpgrade?.call(database, startVersion, endVersion);
             },
             onCreate: (database, version) async {
-              await database.execute(
-                  'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
-
+              await _create(database);
               await callback?.onCreate?.call(database, version);
             },
           );
           return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+        }
+
+        Future<void> _create(sqflite.Database database) async {
+          await database.execute(
+            'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
+        }
+
+        Future<void> _migrate(sqflite.Database database, List<Migration> migrations,
+            int startVersion, int endVersion, Callback? callback) async {
+          try {
+            await MigrationAdapter.runMigrations(
+              database,
+              startVersion,
+              endVersion,
+              migrations,
+            );
+          } on MissingMigrationException catch (_) {
+            throw StateError(
+              'There is no migration supplied to update the database to the current version.'
+              ' Aborting the migration.',
+            );
+          }
         }
       }      
     '''));
   });
 
-  test('open database with DAO', () async {
+  test('open database for simple entity using fallback migration', () async {
     final database = await _createDatabase('''
-      @Database(version: 1, entities: [Person])
-      abstract class TestDatabase extends FloorDatabase {
-        TestDao get testDao;
-      }
-      
-      @entity
-      class Person {
-        @primaryKey
-        final int id;
-      
-        final String name;
-      
-        Person(this.id, this.name);
-      }
-      
-      @dao
-      abstract class TestDao {
-        @insert
-        Future<int> insertPersonWithReturn(Person person);
-      }
-    ''');
-
-    final actual = DatabaseWriter(database).write();
-
-    expect(actual, equalsDart(r'''
-      class _$TestDatabase extends TestDatabase {
-        _$TestDatabase([StreamController<String>? listener]) {
-          changeListener = listener ?? StreamController<String>.broadcast();
-        }
-        
-        TestDao? _testDaoInstance;
-        
-        Future<sqflite.Database> open(String path, List<Migration> migrations,
-            [Callback? callback]) async {
-          final databaseOptions = sqflite.OpenDatabaseOptions(
-            version: 1,
-            onConfigure: (database) async {
-              await database.execute('PRAGMA foreign_keys = ON');
-              await callback?.onConfigure?.call(database);
-            },
-            onOpen: (database) async {
-              await callback?.onOpen?.call(database);
-            },
-            onUpgrade: (database, startVersion, endVersion) async {
-              await MigrationAdapter.runMigrations(
-                  database, startVersion, endVersion, migrations);
-      
-              await callback?.onUpgrade?.call(database, startVersion, endVersion);
-            },
-            onCreate: (database, version) async {
-              await database.execute(
-                  'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
-      
-              await callback?.onCreate?.call(database, version);
-            },
-          );
-          return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
-        }
-      
-        @override
-        TestDao get testDao {
-          return _testDaoInstance ??= _$TestDao(database, changeListener);
-        }
-      }
-    '''));
-  });
-
-  test('open database for complex entity', () async {
-    final database = await _createDatabase('''
-      @Database(version: 1, entities: [Person])
+      @Database(version: 1, entities: [Person], fallbackToDestructiveMigration: true)
       abstract class TestDatabase extends FloorDatabase {}
-      
-      @Entity(tableName: 'custom_table_name')
-      class Person {
-        @PrimaryKey(autoGenerate: true)
-        final int? id;
-      
-        @ColumnInfo(name: 'custom_name')
-        final String name;
-      
-        Person(this.id, this.name);
-      }
-    ''');
-
-    final actual = DatabaseWriter(database).write();
-
-    expect(actual, equalsDart(r'''
-      class _$TestDatabase extends TestDatabase {
-        _$TestDatabase([StreamController<String>? listener]) {
-          changeListener = listener ?? StreamController<String>.broadcast();
-        }
-        
-        Future<sqflite.Database> open(String path, List<Migration> migrations,
-            [Callback? callback]) async {
-          final databaseOptions = sqflite.OpenDatabaseOptions(
-            version: 1,
-            onConfigure: (database) async {
-              await database.execute('PRAGMA foreign_keys = ON');
-              await callback?.onConfigure?.call(database);
-            },
-            onOpen: (database) async {
-              await callback?.onOpen?.call(database);
-            },
-            onUpgrade: (database, startVersion, endVersion) async {
-              await MigrationAdapter.runMigrations(
-                  database, startVersion, endVersion, migrations);
-
-              await callback?.onUpgrade?.call(database, startVersion, endVersion);
-            },
-            onCreate: (database, version) async {
-              await database.execute(
-                  'CREATE TABLE IF NOT EXISTS `custom_table_name` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `custom_name` TEXT NOT NULL)');
-
-              await callback?.onCreate?.call(database, version);
-            },
-          );
-          return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
-        }
-      }      
-    '''));
-  });
-
-  test('open database with view', () async {
-    final database = await _createDatabase('''
-      @Database(version: 1, entities: [Person], views: [Name])
-      abstract class TestDatabase extends FloorDatabase {}
-      
-      @DatabaseView(
-          'SELECT custom_name as name FROM person',
-          viewName: 'names')
-      class Name {
-        final String name;
-      
-        Name(this.name);
-      }
       
       @entity
       class Person {
@@ -216,7 +102,7 @@ void main() {
 
     final actual = DatabaseWriter(database).write();
 
-    expect(actual, equalsDart(r"""
+    expect(actual, equalsDart(r'''
       class _$TestDatabase extends TestDatabase {
         _$TestDatabase([StreamController<String>? listener]) {
          changeListener = listener ?? StreamController<String>.broadcast();
@@ -234,24 +120,301 @@ void main() {
               await callback?.onOpen?.call(database);
             },
             onUpgrade: (database, startVersion, endVersion) async {
-              await MigrationAdapter.runMigrations(
-                  database, startVersion, endVersion, migrations);
-
+              await _migrate(
+                  database, migrations, startVersion, endVersion, callback);
               await callback?.onUpgrade?.call(database, startVersion, endVersion);
             },
             onCreate: (database, version) async {
-              await database.execute(
-                  'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
-                  
-              await database.execute(
-                  'CREATE VIEW IF NOT EXISTS `names` AS SELECT custom_name as name FROM person');
-
+              await _create(database);
               await callback?.onCreate?.call(database, version);
             },
           );
           return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
         }
+
+        Future<void> _create(sqflite.Database database) async {
+          await database.execute(
+            'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
+        }
+
+        Future<void> _migrate(sqflite.Database database, List<Migration> migrations,
+            int startVersion, int endVersion, Callback? callback) async {
+          try {
+            await MigrationAdapter.runMigrations(
+              database,
+              startVersion,
+              endVersion,
+              migrations,
+            );
+          } on Exception catch (exception) {
+            await callback?.onDestructiveUpgrade
+                ?.call(database, startVersion, endVersion, exception);
+            await _dropAll(database);
+            await _create(database);
+          }
+        }
+
+        Future<void> _dropAll(sqflite.Database database) async {
+          await _drop(database, 'table');
+          await _drop(database, 'view');
+        }
+
+        Future<void> _drop(sqflite.Database database, String type) async {
+          final names = await database
+              .rawQuery('SELECT name FROM sqlite_master WHERE type = ?', [type]);
+
+          for (final name in names) {
+            await database.rawQuery('DROP $type ${name['name']}');
+          }
+        }
       }      
+    '''));
+  });
+
+  test('open database with DAO', () async {
+    final database = await _createDatabase('''
+      @Database(version: 1, entities: [Person])
+      abstract class TestDatabase extends FloorDatabase {
+        TestDao get testDao;
+      }
+
+      @entity
+      class Person {
+        @primaryKey
+        final int id;
+
+        final String name;
+
+        Person(this.id, this.name);
+      }
+
+      @dao
+      abstract class TestDao {
+        @insert
+        Future<int> insertPersonWithReturn(Person person);
+      }
+    ''');
+
+    final actual = DatabaseWriter(database).write();
+
+    expect(actual, equalsDart(r'''
+      class _$TestDatabase extends TestDatabase {
+        _$TestDatabase([StreamController<String>? listener]) {
+          changeListener = listener ?? StreamController<String>.broadcast();
+        }
+
+        TestDao? _testDaoInstance;
+
+        Future<sqflite.Database> open(String path, List<Migration> migrations,
+            [Callback? callback]) async {
+          final databaseOptions = sqflite.OpenDatabaseOptions(
+            version: 1,
+            onConfigure: (database) async {
+              await database.execute('PRAGMA foreign_keys = ON');
+              await callback?.onConfigure?.call(database);
+            },
+            onOpen: (database) async {
+              await callback?.onOpen?.call(database);
+            },
+            onUpgrade: (database, startVersion, endVersion) async {
+              await _migrate(
+                  database, migrations, startVersion, endVersion, callback);
+              await callback?.onUpgrade?.call(database, startVersion, endVersion);
+            },
+            onCreate: (database, version) async {
+              await _create(database);
+              await callback?.onCreate?.call(database, version);
+            },
+          );
+          return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+        }
+
+        Future<void> _create(sqflite.Database database) async {
+          await database.execute(
+            'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
+        }
+
+        Future<void> _migrate(sqflite.Database database, List<Migration> migrations,
+            int startVersion, int endVersion, Callback? callback) async {
+          try {
+            await MigrationAdapter.runMigrations(
+              database,
+              startVersion,
+              endVersion,
+              migrations,
+            );
+          } on MissingMigrationException catch (_) {
+            throw StateError(
+              'There is no migration supplied to update the database to the current version.'
+              ' Aborting the migration.',
+            );
+          }
+        }
+
+        @override
+        TestDao get testDao {
+          return _testDaoInstance ??= _$TestDao(database, changeListener);
+        }
+      }
+    '''));
+  });
+
+  test('open database for complex entity', () async {
+    final database = await _createDatabase('''
+      @Database(version: 1, entities: [Person])
+      abstract class TestDatabase extends FloorDatabase {}
+
+      @Entity(tableName: 'custom_table_name')
+      class Person {
+        @PrimaryKey(autoGenerate: true)
+        final int? id;
+
+        @ColumnInfo(name: 'custom_name')
+        final String name;
+
+        Person(this.id, this.name);
+      }
+    ''');
+
+    final actual = DatabaseWriter(database).write();
+
+    expect(actual, equalsDart(r'''
+      class _$TestDatabase extends TestDatabase {
+        _$TestDatabase([StreamController<String>? listener]) {
+          changeListener = listener ?? StreamController<String>.broadcast();
+        }
+
+        Future<sqflite.Database> open(String path, List<Migration> migrations,
+            [Callback? callback]) async {
+          final databaseOptions = sqflite.OpenDatabaseOptions(
+            version: 1,
+            onConfigure: (database) async {
+              await database.execute('PRAGMA foreign_keys = ON');
+              await callback?.onConfigure?.call(database);
+            },
+            onOpen: (database) async {
+              await callback?.onOpen?.call(database);
+            },
+            onUpgrade: (database, startVersion, endVersion) async {
+              await _migrate(
+                  database, migrations, startVersion, endVersion, callback);
+              await callback?.onUpgrade?.call(database, startVersion, endVersion);
+            },
+            onCreate: (database, version) async {
+              await _create(database);
+              await callback?.onCreate?.call(database, version);
+            },
+          );
+          return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+        }
+
+        Future<void> _create(sqflite.Database database) async {
+          await database.execute(
+              'CREATE TABLE IF NOT EXISTS `custom_table_name` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `custom_name` TEXT NOT NULL)');
+        }
+
+        Future<void> _migrate(sqflite.Database database, List<Migration> migrations,
+            int startVersion, int endVersion, Callback? callback) async {
+          try {
+            await MigrationAdapter.runMigrations(
+              database,
+              startVersion,
+              endVersion,
+              migrations,
+            );
+          } on MissingMigrationException catch (_) {
+            throw StateError(
+              'There is no migration supplied to update the database to the current version.'
+              ' Aborting the migration.',
+            );
+          }
+        }
+      }
+    '''));
+  });
+
+  test('open database with view', () async {
+    final database = await _createDatabase('''
+      @Database(version: 1, entities: [Person], views: [Name])
+      abstract class TestDatabase extends FloorDatabase {}
+
+      @DatabaseView(
+          'SELECT custom_name as name FROM person',
+          viewName: 'names')
+      class Name {
+        final String name;
+
+        Name(this.name);
+      }
+
+      @entity
+      class Person {
+        @primaryKey
+        final int id;
+
+        final String name;
+
+        Person(this.id, this.name);
+      }
+    ''');
+
+    final actual = DatabaseWriter(database).write();
+
+    expect(actual, equalsDart(r"""
+      class _$TestDatabase extends TestDatabase {
+        _$TestDatabase([StreamController<String>? listener]) {
+         changeListener = listener ?? StreamController<String>.broadcast();
+        }
+
+        Future<sqflite.Database> open(String path, List<Migration> migrations,
+            [Callback? callback]) async {
+          final databaseOptions = sqflite.OpenDatabaseOptions(
+            version: 1,
+            onConfigure: (database) async {
+              await database.execute('PRAGMA foreign_keys = ON');
+              await callback?.onConfigure?.call(database);
+            },
+            onOpen: (database) async {
+              await callback?.onOpen?.call(database);
+            },
+            onUpgrade: (database, startVersion, endVersion) async {
+              await _migrate(
+                  database, migrations, startVersion, endVersion, callback);
+              await callback?.onUpgrade?.call(database, startVersion, endVersion);
+            },
+            onCreate: (database, version) async {
+              await _create(database);
+              await callback?.onCreate?.call(database, version);
+            },
+          );
+          return sqfliteDatabaseFactory.openDatabase(path, options: databaseOptions);
+        }
+
+        Future<void> _create(sqflite.Database database) async {
+          await database.execute(
+                  'CREATE TABLE IF NOT EXISTS `Person` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY (`id`))');
+
+          await database.execute(
+              'CREATE VIEW IF NOT EXISTS `names` AS SELECT custom_name as name FROM person');
+        }
+
+        Future<void> _migrate(sqflite.Database database, List<Migration> migrations,
+            int startVersion, int endVersion, Callback? callback) async {
+          try {
+            await MigrationAdapter.runMigrations(
+              database,
+              startVersion,
+              endVersion,
+              migrations,
+            );
+          } on MissingMigrationException catch (_) {
+            throw StateError(
+              'There is no migration supplied to update the database to the current version.'
+              ' Aborting the migration.',
+            );
+          }
+        }
+      }
     """));
   });
 }
