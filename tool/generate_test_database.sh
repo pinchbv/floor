3e814f250c27e4e0fde360710ad6388710a85663
Amir Panahandeh <amirpanahandeh@yahoo.com>
2022-04-09 23:15:23 +0430

    Changed 'flat' to 'flat_orm' in tools

--- tool/generate_test_database.sh
+++ tool/generate_test_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ../flat
+cd ../flat_orm
 flutter packages pub run build_runner build --delete-conflicting-outputs
--- tool/upgrade.sh
+++ tool/upgrade.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-cd flat
+cd flat_orm
 flutter packages pub upgrade
 cd ..
 
