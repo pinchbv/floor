64f1eb1e934038f6fc612e8032ea0e5b4dfdfb8b
mirland <mirland@xmartlabs.com>
2023-01-27 11:53:46 -0300

    Web support

--- floor/lib/src/sqflite_database_factory.dart
+++ floor/lib/src/sqflite_database_factory.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 
 // infers factory as nullable without explicit type definition
 final DatabaseFactory sqfliteDatabaseFactory = () {
-  if (Platform.isAndroid || Platform.isIOS) {
+  if (kIsWeb){
+    return databaseFactoryFfiWeb;
+  } else if (Platform.isAndroid || Platform.isIOS) {
     return databaseFactory;
   } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
     sqfliteFfiInit();
