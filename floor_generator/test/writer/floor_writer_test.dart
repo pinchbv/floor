b8a30815c744333a50549efa3afd01f3a4cfbdba
Stephan Veenstra <stephan.veenstra@pinch.nl>
2023-07-19 11:07:56 +0200

    Reorder ignore rules on generated class

--- floor_generator/test/writer/floor_writer_test.dart
+++ floor_generator/test/writer/floor_writer_test.dart
@@ -13,8 +13,8 @@ void main() {
     final actual = FloorWriter(databaseName).write();
 
     expect(actual, equalsDart(r'''
-      // ignore: avoid_classes_with_only_static_members
       // ignore_for_file: library_private_types_in_public_api
+      // ignore: avoid_classes_with_only_static_members
       class $FloorFooBar {
         /// Creates a database builder for a persistent database.
         /// Once a database is built, you should keep a reference to it and re-use it.
