aca286c1eb49b0dde80b722e6f217e06d8dbcc3b
hendrikvanderkaaden <hendrikvdkaaden@gmail.com>
2024-03-06 09:38:14 +0100

    Deleted test not relevant anymore

--- floor_generator/test/misc/extension/type_converter_element_extension_test.dart
+++ floor_generator/test/misc/extension/type_converter_element_extension_test.dart
@@ -37,19 +37,6 @@ void main() {
       expect(actual, equals({expected}));
     });
 
-    test('throws error when null in annotation', () async {
-      const typeConverterScope = TypeConverterScope.database;
-      final element = await '''
-        @TypeConverters(null)
-        abstract class Foo {}
-      '''
-          .asClassElement();
-
-      final actual = () => element.getTypeConverters(typeConverterScope);
-
-      expect(actual, throwsProcessorError());
-    });
-
     test('throws error when empty list in annotation', () async {
       const typeConverterScope = TypeConverterScope.database;
       final element = await '''
