788502ed680b4465bb810c28605f673519712983
Stephan Mantel <stephan.mantel@pinch.nl>
2024-05-24 13:16:32 +0200

    Run analyse using foreach_module script

--- tool/analyze.sh
+++ tool/analyze.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-folders=(
-  example
-  floor
-  floor_annotation
-  floor_common
-  floor_ffi
-  floor_generator
-)
-
-cd ..
-
-for folder in "${folders[@]}"
-do
-  cd "$folder" || exit;
-  dart analyze .
-  cd ..
-done
+./foreach_module.sh 'dart analyze .'
